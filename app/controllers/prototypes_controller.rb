43a55f7abe0daaef9bfdf2a881ac95699c4dd907
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-03-23 15:24:28 +0900

    新規投稿保存機能追加

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -6,4 +6,16 @@ class PrototypesController < ApplicationController
   def new
     @prototypes = Prototype.new
   end
+
+  def create
+    Prototype.create(prototype_params)
+    redirect_to action: :index
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept)
+  end
+
 end
