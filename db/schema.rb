9609108a41e742843744086bbbc3880093f7863d
YuyaAbe0331 <you8.kevin@gmail.com>
2023-03-24 20:23:41 +0900

    マイグレーションファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2023_03_24_074800) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -48,9 +47,9 @@ ActiveRecord::Schema.define(version: 2023_03_24_074800) do
     t.string "title", null: false
     t.text "catch_copy", null: false
     t.text "concept", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
@@ -71,7 +70,6 @@ ActiveRecord::Schema.define(version: 2023_03_24_074800) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-
   add_foreign_key "comments", "prototypes"
   add_foreign_key "comments", "users"
   add_foreign_key "prototypes", "users"
