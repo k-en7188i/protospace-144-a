593a830b7492a9c54ee61364d8c95c732a24572c
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-03-24 20:54:42 +0900

    ユーザID追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2023_03_24_074800) do
+ActiveRecord::Schema.define(version: 2023_03_24_091856) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -71,7 +70,6 @@ ActiveRecord::Schema.define(version: 2023_03_24_074800) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-
   add_foreign_key "comments", "prototypes"
   add_foreign_key "comments", "users"
   add_foreign_key "prototypes", "users"
