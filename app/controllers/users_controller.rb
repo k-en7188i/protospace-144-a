3a6cfddd5a24d5fcd6a50f53d01a202fb5fe3177
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-03-27 20:03:01 +0900

    ユーザー詳細画面遷移設定、部分テンプレート

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
 
-def show
-  @name = current_user.name
-  @profile = current_user.profile
-  @occupation = current_user.occupation
-  @position = current_user.position
-  @prototypes = current_user.prototypes
-end
-
+  def show
+    @user = User.find(params[:id])
+    @prototypes = @user.prototypes
+  end
 end
