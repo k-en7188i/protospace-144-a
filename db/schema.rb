de7997a273437fec4af97635d1d0c95ed3798103
YuyaAbe0331 <you8.kevin@gmail.com>
2023-03-27 12:05:24 +0900

    データベースのリセット

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2023_03_24_091856) do
+ActiveRecord::Schema.define(version: 2023_03_24_074800) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
